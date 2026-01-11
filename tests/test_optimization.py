9f001307f034d10521009f5958948679739dbb70
Ilia-Luchnikov <luchnikovilya@gmail.com>
2026-01-11 21:18:27 +0400

    optimization test completed

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,4 +1,17 @@
+from itertools import product
 from mqlib_wrap import run_heuristics, get_energy_function
+from mqlib_wrap.config import analyse_and_desug_config
+
+
+def get_all_states_iter(nodes_number):
+    return product((1, -1), repeat=nodes_number)
+
+def minimize_bruteforce(config):
+    config = analyse_and_desug_config(config)
+    nodes_number = config["nodes_number"]
+    energy_function = get_energy_function(config)
+    return min(map(lambda s: (energy_function(s), list(s)), get_all_states_iter(nodes_number)), key = lambda x: x[0])
+    
 
 def test_optimization():
     config = {
@@ -18,3 +31,9 @@ def test_optimization():
     for _, result in results.items():
         energy, configuration = result["energy"], result["configuration"]
         assert abs(energy - energy_function(configuration)) < 1e-10
+    correct_min, correct_argmin = minimize_bruteforce(config)
+    best_result = min(results.items(), key = lambda x: x[1]["energy"])
+    best_min = best_result[1]["energy"]
+    best_argmin = best_result[1]["configuration"]
+    assert abs(correct_min - best_min) < 1e-10
+    assert correct_argmin == best_argmin
